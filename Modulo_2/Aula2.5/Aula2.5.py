d8c1045bd5ff1388cf52fb1800fa60606b0ce0dd
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:27:10 -0300

    Modulo 2 - Aula 2.5 (Aula sobre o PRINT)

---
+++ Modulo_2/Aula2.5/Aula2.5.py
@@ -0,0 +1,32 @@
+
+print(123456)
+
+print('Rafael', 'Rabelo')  # ',' no ja vem com espaço.
+
+# 'sep=' serve para colocar o espaço com caractere a sua escolha
+# Exemplo 1, Apenas 1 espaço.
+print('Rafael', 'Rabelo', sep='-')
+
+# 'sep=' serve para colocar o espaço com caractere a sua escolha
+# Exemplo 2, Com varios espaços.
+print('Roger', 'e', 'Rafael', sep='|')
+
+# sep=' serve para colocar ou retiraço o espaço com caractere ou sem caractere a sua escolha.
+# Exemplo 3, END e SEP,
+print('Rafael', 'Rabelo', sep='-', end='|')
+print('Roger', 'e', 'Rafael', sep='-', end='*')
+
+# ---------- Python é case sensitive ----------
+
+# Print de um CPF - BASICO
+
+print('')
+print('123', end='.')
+print('321', end='.')
+print('213', end='-')
+print('13')
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
+
+# Final
