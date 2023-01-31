b97cf41f814f153509add3ce0324bba03e7aea45
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:42:23 -0300

    Modulo 2 - Aula 2.25 (Aula Len - Quantidade de caracteres)

---
+++ Modulo_2/Aula2.25/Aula2.25.py
@@ -0,0 +1,48 @@
+#
+# Verificar quantidade caracteres tem dentro de uma STR
+#
+# len - Coletando quantidade de caracteres
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Exemplo 1
+# usuario = input("Digite seu usuário: ")
+# qtdcaracteres = len(usuario)
+# print(usuario, qtdcaracteres, type(qtdcaracteres))
+#
+# if 8 > qtdcaracteres or 16 < qtdcaracteres:
+#     print("Digite um username com > 8 e < 16")
+#     usuario = input("Digite outro usuário: ")
+#     qtdcaracteres = len(usuario)
+#     print(usuario, qtdcaracteres, type(qtdcaracteres))
+# else:
+#     print("Cadastrado")
+
+# ----------------------------------------------------------------------------------------------------------------------
+# # Exemplo 2
+# usuario = input("Digite seu usuário: ")
+# qtdcaracteres = len(usuario)
+# print(usuario, qtdcaracteres, type(qtdcaracteres))
+#
+# if 8 > qtdcaracteres or 16 < qtdcaracteres:
+#     print("Digite um username com > 8 e < 16")
+#     usuario = input("Digite outro usuário: ")
+#     qtdcaracteres = len(usuario)
+#     print(usuario, qtdcaracteres, type(qtdcaracteres))
+# else:
+#     print("Cadastrado")
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Exemplo 3 - Somar caracteres
+# nome1 = input("Digite primeiro nome: ")
+# nome2 = input("Digite segundo nome: ")
+# nome3 = input("Digite terceiro nome: ")
+#
+# print("1º nome, total de letras: ", len(nome1))
+# print("2º nome, total de letras: ", len(nome2))
+# print("3º nome, total de letras: ", len(nome3))
+#
+# print(f'Total de letras do seu nome completo {len(nome1) + len(nome2) + len(nome3)}')
+
+
+
+
