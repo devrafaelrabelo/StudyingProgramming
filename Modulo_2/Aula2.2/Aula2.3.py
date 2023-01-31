a320b661b040ca50cef956617b2d5cb02e2fc532
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:26:04 -0300

    Modulo 2 - Aula 2.3 (Aula de Comentario)

---
+++ Modulo_2/Aula2.2/Aula2.3.py
@@ -0,0 +1,20 @@
+# Aula1.1 de Comentário
+
+print("Hello World!")
+# Comentario "#"
+
+print("Linha 1")
+print("Linha 2")
+# Comentario Lembrando
+# Linha 2 Comentario
+print("Linha 3")  # Comentario na Frente do comando, sempre usar dois espaço antes do #
+# print("Qualquer Coisa")
+print("Linha 5")
+print("Linha 6")
+# Comentario em bloco
+""" 
+print("Linha 7") 
+print("Linha 8")
+print("Linha 9")
+"""
+print("Linha 10")  # Final
