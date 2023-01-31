4bb7afe0849b044a2333a3ec61e4681f2f5a49e8
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:28:54 -0300

    Modulo 2 - Aula 2.7 (Aula String e Aspas)

---
+++ Modulo_2/Aula2.7/Aula2.7.py
@@ -0,0 +1,20 @@
+"""
+
+str - String
+
+"""
+
+
+print('Teste Aspas Simples')  # Aspas Simples
+print("Teste Aspas Duplas")  # Aspas Duplas
+print(12356)  # Numero Inteiro
+
+
+print('Essa é uma "string" (str).')
+print("Essa é uma 'string' (str).")
+print("Essa é uma \"string\" (str).")  # Barra Invertida antes serve para printar varias aspas dentro do print
+print('Essa é uma \'string\' (str).')
+print('Essa é uma \n \'string\' (str).')  # Barra Invertida + N - (\n), Serve para quebra de linha.
+print(r'Essa é uma \n \'string\' (str).')  # r antes das aspas, serve para printar sem nenhuma função.
+
+# Final
