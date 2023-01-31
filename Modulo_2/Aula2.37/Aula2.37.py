0e92d072cded102d481e257321cd8fea9ec2d653
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:52:29 -0300

    Modulo 2 - Aula 2.37 (Aula FOR, ELSE)

---
+++ Modulo_2/Aula2.37/Aula2.37.py
@@ -0,0 +1,34 @@
+#
+# FOR / ELSE em Pyhton
+#
+
+variavel = ['Rafael Rabelo', 'Rayane Riberio', 'Violeta Rabelo']
+#comeca_r = False
+
+# for valor in variavel:
+#     if valor.startswith('R'):
+#         print('Começa com R')
+#     else:
+#         print("Não começa com R")
+
+# for valor in variavel:
+#     if valor.lower().startswith('v'):
+#         comeca_r = True
+#
+# if comeca_r:
+#     print('Existe uma palavra com V.')
+# else:
+#     print('Não Existe.')
+
+#
+# for valor in variavel:
+#     if not valor.lower().startswith('r'):
+#         continue
+#     print(valor)
+
+
+# for valor in variavel:
+#     if 'r' not in valor.lower():
+#         continue
+#     print(valor)
+
