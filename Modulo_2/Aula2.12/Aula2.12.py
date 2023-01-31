a663b347eb259d2f171c93700a928605a79ebf8e
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:32:29 -0300

    Modulo 2 - Aula 2.12 (Aula Solucao dos exercicios)

---
+++ Modulo_2/Aula2.12/Aula2.12.py
@@ -0,0 +1,16 @@
+# Resolução do Exerc Aula1.1 2,11
+
+nome = "Rafael = "  # Declarar variavel.
+idade = 27
+altura = 1.81
+maior_de_idade = idade >= 18
+peso = 63
+imc = 0.0
+
+print("Nome:  ", nome, type(nome))
+print("Idade: ", idade, type(idade))
+print("Altura: ", altura, type(altura))
+print("É maior de Idade: ", maior_de_idade, type(maior_de_idade))
+print("Peso: ", peso, type(peso))
+
+print(nome, 'tem ', idade, 'anos e seu imc é ', peso / altura ** 2)
