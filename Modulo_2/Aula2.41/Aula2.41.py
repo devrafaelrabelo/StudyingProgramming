a21d21997c1dd8c9375448b20599efff23c2cac2
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:55:21 -0300

    Modulo 2 - Aula 2.41 (Aula Operacao Ternaria)

---
+++ Modulo_2/Aula2.41/Aula2.41.py
@@ -0,0 +1,57 @@
+#
+# Operador ternário em PYTHON
+#
+
+# logger_used = True
+#
+# if logger_used:
+#     msg = "Usuário logado."
+# else:
+#     msg = "Usuário precisa logar"
+#
+# print(msg)
+
+# logger_used = False
+# msg = "Usuário logado." if logger_used else 'Usuário precisa logar.'
+#
+# print(logger_used)
+# print(msg)
+#
+# logger_used = True
+# msg = "Usuário logado." if logger_used else 'Usuário precisa logar.'
+# print(msg)
+# # print(logger_used)
+
+# idade = 18
+# msg = "Pode acessar." if idade >= 18 else 'Não pode acessar'
+#
+# print(idade)
+# print(msg)
+#
+# idade = 18
+#
+# msg = "Pode acessar." if idade >= 18 else 'Não pode acessar'
+#
+# print(idade)
+# print(msg)
+# print(idade)
+
+# while True:
+#     idade = input("Digite sua IDADE: ")
+#     if not idade.isnumeric():
+#         print(f'"{idade}" não é valido')
+#         print(f'Digite um numero valido')
+#         continue
+#
+#     msg = "Pode acessar." if int(idade) >= 18 else 'Não pode acessar'
+#     print(msg)
+
+# while True:
+#     idade = input("Digite sua IDADE: ")
+#     if not idade.isnumeric():
+#         print(f'"{idade}" não é valido')
+#         print(f'Digite um numero valido')
+#         continue
+#
+#     msg = "Pode acessar." if 18 <= int(idade) <= 60 else 'Não pode acessar'
+#     print(msg)
