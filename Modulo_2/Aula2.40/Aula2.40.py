1b8c62e2c17783f2ef8931a789f378b5c31a4e83
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:54:27 -0300

    Modulo 2 - Aula 2.40 (Aula Trocando o valor entre variaveis)

---
+++ Modulo_2/Aula2.40/Aula2.40.py
@@ -0,0 +1,24 @@
+#
+# Invertendo valores
+#
+# Ex - x -> y
+#     y -> x
+#
+# Exemplo
+x = 10  # Luiz
+y = 'Luiz'  # 10
+z = 'Otavio'
+#
+# print(f'x = {x} e y = {y}\n')
+#
+# z = x
+# x = y
+# y = z
+#
+# print(f'x = {x} e y = {y}')
+
+print(f'x = {x} e y = {y} z = {z}\n')
+
+x, y, z = z, x, y
+
+print(f'x = {x} e y = {y} z = {z}\n')
