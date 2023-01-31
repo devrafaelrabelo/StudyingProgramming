e51cbb03f34e85feda44f3dc9934e30fdc576f04
devrafaelrabelo <devrafaelrabelo@gmail.com>
2023-01-31 10:44:09 -0300

    Modulo 2 - Aula 2.27 (Aula Pass e Ellipsis como placeholders)

---
+++ Modulo_2/Aula2.27/Aula2.27.py
@@ -0,0 +1,13 @@
+#
+# Pass ( pass ) & Ellpsis ( ... ) - Como placeholders
+#
+
+# Exemplo
+valor = True
+
+if valor:
+    # usar um dos dois, serve para segurar o local para escrever depois o codigo.
+    pass  # o Comando foi feito especialmente para esse trabalho
+    ...
+else:
+    print('Tchau')
