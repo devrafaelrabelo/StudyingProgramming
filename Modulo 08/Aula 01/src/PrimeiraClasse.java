b6f509d69fdc7c5ed4aaa221053f6c94b1389888
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-07-05 09:09:30 -0300

    Modulo 08 - Aula 01 (Tipo de variáveis básicas) Final

---
+++ Modulo 08/Aula 01/src/PrimeiraClasse.java
@@ -0,0 +1,21 @@
+public class PrimeiraClasse {
+
+    private int codigo;
+
+    private long codigoMaior;
+
+    private double valorDecimalG;
+
+    private String texto;
+
+    private boolean status;
+
+    // Menos Usados
+    private float valorDecimalP;
+    private short shor;
+
+    private byte bit;
+
+
+
+}
