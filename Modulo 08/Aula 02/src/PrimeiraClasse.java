af7fa05bc643c950516fdb9a475a1826ce515d0f
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-07-05 09:23:06 -0300

    Modulo 08 - Aula 02 (Variáveis de classes e métodos)

---
+++ Modulo 08/Aula 02/src/PrimeiraClasse.java
@@ -0,0 +1,32 @@
+public class PrimeiraClasse {
+
+        private int codigo = 20;
+
+        private long codigoMaior = 132456;
+
+        private double valorDecimalG = 123.00
+
+        private String texto = "TEXTO TESTE 123456789";
+
+        private boolean status = false;
+
+        // Menos Usados
+        private float valorDecimalP;
+
+        private short shor;
+
+        private byte bit;
+
+        public String retornarTexto() {
+                return "TESTE TEXTO";
+        }
+
+        public int retornarInt(){
+                int val = 50;
+                return val;
+        }
+
+        public long retornarLong() {
+                return 123456789;
+        }
+}
