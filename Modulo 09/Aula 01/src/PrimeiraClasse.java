10455d78e75b74dd3b3e77a890c11eecc9a3d26f
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-07-05 13:02:43 -0300

    Modulo 09 - Aula 01 (Tipos complexos de variáveis (Wrappers))

---
+++ Modulo 09/Aula 01/src/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+public class PrimeiraClasse {
+
+    private Integer codigo;
+
+    private Long codigoMaior = 123456789l;
+
+    private Double valorDecimalG = 123.12;
+
+    private Boolean status = false;
+}
