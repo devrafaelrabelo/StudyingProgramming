61ffd6008e3d7cb400ff238caf4bc7eece8c7943
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-07-05 11:13:53 -0300

    Modulo 08 - Aula 04 (Exercicios) Final

---
+++ Modulo 08/Aula 04/src/Exercicios.java
@@ -0,0 +1,21 @@
+public class Exercicios {
+
+    public static void main(String arg[]){
+
+        mediaNotas();
+
+    }
+
+    private static void mediaNotas() {
+        int nota1 = 10;
+        int nota2 = 12;
+        int nota3 = 15;
+        int nota4 = 8;
+        double resul = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Media das notas dos alunos é: " + resul);
+
+
+    }
+
+}
