a7b1913b88c4207f4b1dd18e6f08d797eb034457
Rafael Rabelo <devrafaelrabelo@gmail.com>
2023-08-10 10:34:40 -0300

    Deu Zebra 2

---
+++ Modulo 07/Modulo 07 Aula 04/src/Cliente.java
@@ -0,0 +1,40 @@
+public class Cliente {
+
+    private int Codigo;
+
+    private String nome;
+
+    private String endereco;
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        Codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void cadastrarEndereco(String endereco) {
+        setEndereco(endereco);
+    }
+
+    public void imprimirEndereco() {
+        System.out.println(this.endereco);
+    }
+}
